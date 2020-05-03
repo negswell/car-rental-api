4d875a0a4a76b866b3bf6b9dea3caeaedb7abdb1
unknown <negiaditya16@gmail.com>
2020-05-03 11:05:45 +0530

    fix

--- models/database.js
+++ models/database.js
@@ -1,6 +1,6 @@
 let mongoose = require('mongoose') 
 
-const URI= process.env.URI || 'mongodb://heroku_vxx40qq7:f185n9vbmv2l1i54e61e92vn6q@ds133875.mlab.com:33875/heroku_vxx40qq7'
+const URI= process.env.MONGODB_URI
 
 console.log(URI) ;
 mongoose.connect(URI,{useNewUrlParser: true, useUnifiedTopology: true}) ;
